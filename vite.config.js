5a9ecc09c3bdaf9939127b52d29bb5bd95e0fcd5
Hirako San <hirako2000@gmail.com>
2024-06-04 14:24:47 +0700

    lints

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    root: 'src',
-     base: '/',
-    build: {
-        outDir: '../dist',
-        rollupOptions: {
-            input: ['src/index.html', 'src/oni.html', 'src/deschool.html', 'src/uploads.html']
-        }
-    }
+	root: "src",
+	base: "/",
+	build: {
+		outDir: "../dist",
+		rollupOptions: {
+			input: [
+				"src/index.html",
+				"src/oni.html",
+				"src/deschool.html",
+				"src/uploads.html",
+			],
+		},
+	},
 });
